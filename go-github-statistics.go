cf57e908e1ea1fd30e09b6e8e8f093cad9a2bc7d
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-03-10 14:22:59 +0100

    v.0.1 is ready

--- go-github-statistics.go
+++ go-github-statistics.go
@@ -1,5 +1,79 @@
+// Copyright 2018 Fedir RYKHTIK. All rights reserved.
+// Use of this source code is governed by the GNU GPL 3.0
+// license that can be found in the LICENSE file.
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
 
+type Match struct {
+	Name       string    `json:"name"`
+	FullName   string    `json:"full_name"`
+	Watchers   int       `json:"watchers"`
+	Forks      int       `json:"forks"`
+	OpenIssues int       `json:"open_issues"`
+	CreatedAt  time.Time `json:"created_at"`
+}
+
+func getRemoteJSON(repoKey string) []byte {
+	url := "https://api.github.com/repos/" + repoKey
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	jsonResponse, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return jsonResponse
+}
+
+func parseJSON(jsonResponse []byte) *Match {
+	result := &Match{}
+	err := json.Unmarshal([]byte(jsonResponse), result)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func PrepareResult(res *Match) string {
+	return fmt.Sprintf("\tName: %s\n", res.Name) +
+		fmt.Sprintf("\tFull name: %s\n", res.FullName) +
+		fmt.Sprintf("\tCreated at: %d/%02d\n", res.CreatedAt.Year(), res.CreatedAt.Month()) +
+		fmt.Sprintf("\tStars: %d\n", res.Watchers) +
+		fmt.Sprintf("\tForks: %d\n", res.Forks) +
+		fmt.Sprintf("\tOpen issues : %d\n", res.OpenIssues)
+}
+
+func PrintResult(res *Match) {
+	fmt.Println(PrepareResult(res))
+}
+
+func PrintRepositoryStatistics(RepoKey string) {
+	PrintResult(parseJSON(getRemoteJSON(RepoKey)))
+}
+
+func main() {
+	PrintRepositoryStatistics("astaxie/beego")
+	PrintRepositoryStatistics("gobuffalo/buffalo")
+	PrintRepositoryStatistics("go-chi/chi")
+	PrintRepositoryStatistics("gohugoio/hugo")
+	PrintRepositoryStatistics("labstack/echo")
+	PrintRepositoryStatistics("revel/revel")
+	PrintRepositoryStatistics("gin-gonic/gin")
+	PrintRepositoryStatistics("kataras/iris")
 }
---
+++ go-github-statistics_test.go
@@ -0,0 +1,116 @@
+// Copyright 2018 Fedir RYKHTIK. All rights reserved.
+// Use of this source code is governed by the GNU GPL 3.0
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"testing"
+)
+
+// TODO :)
+func TestJSONDataExtraction(t *testing.T) {
+	jsonResponse := `{
+		"id": 3577919,
+		"name": "beego",
+		"full_name": "astaxie/beego",
+		"owner": {
+		  "login": "astaxie",
+		  "id": 233907,
+		  "avatar_url": "https://avatars3.githubusercontent.com/u/233907?v=4",
+		  "gravatar_id": "",
+		  "url": "https://api.github.com/users/astaxie",
+		  "html_url": "https://github.com/astaxie",
+		  "followers_url": "https://api.github.com/users/astaxie/followers",
+		  "following_url": "https://api.github.com/users/astaxie/following{/other_user}",
+		  "gists_url": "https://api.github.com/users/astaxie/gists{/gist_id}",
+		  "starred_url": "https://api.github.com/users/astaxie/starred{/owner}{/repo}",
+		  "subscriptions_url": "https://api.github.com/users/astaxie/subscriptions",
+		  "organizations_url": "https://api.github.com/users/astaxie/orgs",
+		  "repos_url": "https://api.github.com/users/astaxie/repos",
+		  "events_url": "https://api.github.com/users/astaxie/events{/privacy}",
+		  "received_events_url": "https://api.github.com/users/astaxie/received_events",
+		  "type": "User",
+		  "site_admin": false
+		},
+		"private": false,
+		"html_url": "https://github.com/astaxie/beego",
+		"description": "beego is an open-source, high-performance web framework for the Go programming language.",
+		"fork": false,
+		"url": "https://api.github.com/repos/astaxie/beego",
+		"forks_url": "https://api.github.com/repos/astaxie/beego/forks",
+		"keys_url": "https://api.github.com/repos/astaxie/beego/keys{/key_id}",
+		"collaborators_url": "https://api.github.com/repos/astaxie/beego/collaborators{/collaborator}",
+		"teams_url": "https://api.github.com/repos/astaxie/beego/teams",
+		"hooks_url": "https://api.github.com/repos/astaxie/beego/hooks",
+		"issue_events_url": "https://api.github.com/repos/astaxie/beego/issues/events{/number}",
+		"events_url": "https://api.github.com/repos/astaxie/beego/events",
+		"assignees_url": "https://api.github.com/repos/astaxie/beego/assignees{/user}",
+		"branches_url": "https://api.github.com/repos/astaxie/beego/branches{/branch}",
+		"tags_url": "https://api.github.com/repos/astaxie/beego/tags",
+		"blobs_url": "https://api.github.com/repos/astaxie/beego/git/blobs{/sha}",
+		"git_tags_url": "https://api.github.com/repos/astaxie/beego/git/tags{/sha}",
+		"git_refs_url": "https://api.github.com/repos/astaxie/beego/git/refs{/sha}",
+		"trees_url": "https://api.github.com/repos/astaxie/beego/git/trees{/sha}",
+		"statuses_url": "https://api.github.com/repos/astaxie/beego/statuses/{sha}",
+		"languages_url": "https://api.github.com/repos/astaxie/beego/languages",
+		"stargazers_url": "https://api.github.com/repos/astaxie/beego/stargazers",
+		"contributors_url": "https://api.github.com/repos/astaxie/beego/contributors",
+		"subscribers_url": "https://api.github.com/repos/astaxie/beego/subscribers",
+		"subscription_url": "https://api.github.com/repos/astaxie/beego/subscription",
+		"commits_url": "https://api.github.com/repos/astaxie/beego/commits{/sha}",
+		"git_commits_url": "https://api.github.com/repos/astaxie/beego/git/commits{/sha}",
+		"comments_url": "https://api.github.com/repos/astaxie/beego/comments{/number}",
+		"issue_comment_url": "https://api.github.com/repos/astaxie/beego/issues/comments{/number}",
+		"contents_url": "https://api.github.com/repos/astaxie/beego/contents/{+path}",
+		"compare_url": "https://api.github.com/repos/astaxie/beego/compare/{base}...{head}",
+		"merges_url": "https://api.github.com/repos/astaxie/beego/merges",
+		"archive_url": "https://api.github.com/repos/astaxie/beego/{archive_format}{/ref}",
+		"downloads_url": "https://api.github.com/repos/astaxie/beego/downloads",
+		"issues_url": "https://api.github.com/repos/astaxie/beego/issues{/number}",
+		"pulls_url": "https://api.github.com/repos/astaxie/beego/pulls{/number}",
+		"milestones_url": "https://api.github.com/repos/astaxie/beego/milestones{/number}",
+		"notifications_url": "https://api.github.com/repos/astaxie/beego/notifications{?since,all,participating}",
+		"labels_url": "https://api.github.com/repos/astaxie/beego/labels{/name}",
+		"releases_url": "https://api.github.com/repos/astaxie/beego/releases{/id}",
+		"deployments_url": "https://api.github.com/repos/astaxie/beego/deployments",
+		"created_at": "2012-02-29T02:32:08Z",
+		"updated_at": "2018-03-10T10:12:31Z",
+		"pushed_at": "2018-03-10T09:17:53Z",
+		"git_url": "git://github.com/astaxie/beego.git",
+		"ssh_url": "git@github.com:astaxie/beego.git",
+		"clone_url": "https://github.com/astaxie/beego.git",
+		"svn_url": "https://github.com/astaxie/beego",
+		"homepage": "beego.me",
+		"size": 4396,
+		"stargazers_count": 14389,
+		"watchers_count": 14389,
+		"language": "Go",
+		"has_issues": true,
+		"has_projects": true,
+		"has_downloads": true,
+		"has_wiki": true,
+		"has_pages": false,
+		"forks_count": 3166,
+		"mirror_url": null,
+		"archived": false,
+		"open_issues_count": 448,
+		"license": {
+		  "key": "other",
+		  "name": "Other",
+		  "spdx_id": null,
+		  "url": null
+		},
+		"forks": 3166,
+		"open_issues": 448,
+		"watchers": 14389,
+		"default_branch": "master",
+		"network_count": 3166,
+		"subscribers_count": 1042
+	  }
+	  `
+	JSON := getContent()
+	expectedOutput := JSON
+	if expectedOutput != JSON {
+		t.Fail()
+	}
+}
