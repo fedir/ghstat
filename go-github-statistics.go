d3e5f3fe652bdf439cf4719d2e4f0faf22554930
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-03-10 16:09:29 +0100

    CSV writer added

--- go-github-statistics.go
+++ go-github-statistics.go
@@ -4,13 +4,30 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
+
+var repositoriesKeys = []string{
+	"astaxie/beego",
+	"gobuffalo/buffalo",
+	"go-chi/chi",
+	"gohugoio/hugo",
+	"labstack/echo",
+	"revel/revel",
+	"gin-gonic/gin",
+	"kataras/iris",
+}
+
+var data = [][]string{}
+
+var csvContent []string
 
 type Match struct {
 	Name       string    `json:"name"`
@@ -46,7 +63,30 @@ func parseJSON(jsonResponse []byte) *Match {
 	return result
 }
 
+func createCsv() {
+	file, err := os.Create("result.csv")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.Write(csvContent)
+	if err != nil {
+		log.Fatal("Cannot write to file", err)
+	}
+}
+
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
+}
+
 func PrepareResult(res *Match) string {
+	csvContent = append(csvContent, fmt.Sprintf("%s,%s,%d/%d,%d,%d,%d", res.Name, res.FullName, res.CreatedAt.Year(), res.CreatedAt.Month(), res.Watchers, res.Forks, res.OpenIssues))
 	return fmt.Sprintf("\tName: %s\n", res.Name) +
 		fmt.Sprintf("\tFull name: %s\n", res.FullName) +
 		fmt.Sprintf("\tCreated at: %d/%02d\n", res.CreatedAt.Year(), res.CreatedAt.Month()) +
@@ -64,12 +104,8 @@ func PrintRepositoryStatistics(RepoKey string) {
 }
 
 func main() {
-	PrintRepositoryStatistics("astaxie/beego")
-	PrintRepositoryStatistics("gobuffalo/buffalo")
-	PrintRepositoryStatistics("go-chi/chi")
-	PrintRepositoryStatistics("gohugoio/hugo")
-	PrintRepositoryStatistics("labstack/echo")
-	PrintRepositoryStatistics("revel/revel")
-	PrintRepositoryStatistics("gin-gonic/gin")
-	PrintRepositoryStatistics("kataras/iris")
+	for _, rKey := range repositoriesKeys {
+		PrintRepositoryStatistics(rKey)
+	}
+	createCsv()
 }
