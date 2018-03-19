2f905912503ccf6ce15e53be48d1c5b4ed797113
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-03-19 22:56:36 +0100

    Go frameworks and PHP frameworks statistic collection launchers

---
+++ bin/go_stats.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./ghstat -f stats/go_frameworks.csv
+
+
---
+++ bin/php_stats.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./ghstat -r laravel/framework,symfony/symfony,yiisoft/yii2,bcit-ci/CodeIgniter -f stats/php_frameworks.csv
+
+
