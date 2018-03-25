74b0bef9243606c9d49361b9c3a550f7bcba6c52
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-03-25 21:00:05 +0200

    Exclude non-github hosted CMS

--- bin/php_cms_stats.sh
+++ bin/php_cms_stats.sh
@@ -1,3 +1,6 @@
-./ghstat -r TYPO3/TYPO3.CMS,Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,drupal/drupal,ezsystems/ezplatform,\
+#./ghstat -r TYPO3/TYPO3.CMS,Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,drupal/drupal,ezsystems/ezplatform,\
+#forkcms/forkcms,getgrav/grav,joomla/joomla-cms,octobercms/october,pagekit/pagekit,redkite-labs/RedKiteCms,roadiz/roadiz,sulu/sulu-standard,\
+#symfony-cmf/symfony-cmf,zikula/core -f stats/php_cms.csv
+./ghstat -r Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,\
 forkcms/forkcms,getgrav/grav,joomla/joomla-cms,octobercms/october,pagekit/pagekit,redkite-labs/RedKiteCms,roadiz/roadiz,sulu/sulu-standard,\
 symfony-cmf/symfony-cmf,zikula/core -f stats/php_cms.csv
