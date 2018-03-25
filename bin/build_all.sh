b603163d22c2095aec836d276bf722fd07f0cd3a
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-03-25 19:48:16 +0200

    PHP configuration

--- bin/build_all.sh
+++ bin/build_all.sh
@@ -1,6 +1,9 @@
+# web frameworks
 bash bin/go_stats.sh
 bash bin/haskell_stats.sh
 bash bin/java_stats.sh
 bash bin/js_stats.sh
 bash bin/php_stats.sh
 bash bin/all_stats.sh
+# php cms
+bash bin/php_cms_stats.sh
