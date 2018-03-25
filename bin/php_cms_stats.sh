eb237c1fc1f06c475f7bface909dccab6a22d2a7
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-03-25 17:50:37 +0200

    PHP CMS statistics launcher

---
+++ bin/php_cms_stats.sh
@@ -0,0 +1,3 @@
+./ghstat -r TYPO3/TYPO3.CMS,Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,drupal/drupal,ezsystems/ezplatform,\
+forkcms/forkcms,getgrav/grav,joomla/joomla-cms,octobercms/october,pagekit/pagekit,redkite-labs/RedKiteCms,roadiz/roadiz,sulu/sulu-standard,\
+symfony-cmf/symfony-cmf,zikula/core -f stats/php_cms.csv
