05e22465566341b34ea0623ee77135890daaa0b2
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-01 01:34:28 +0200

    Adding more Python frameworks

--- bin/python_frameworks.sh
+++ bin/python_frameworks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ghstat -r bottlepy/bottle,plotly/dash,django/django,pallets/flask,Pylons/pyramid,channelcat/sanic,tornadoweb/tornado -f stats/python_frameworks.csv
+./ghstat -r bottlepy/bottle,plotly/dash,django/django,pallets/flask,Pylons/pyramid,channelcat/sanic,tornadoweb/tornado,web2py/web2py -f stats/python_frameworks.csv
