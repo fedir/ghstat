6fd3816a5c0a2bb8d941d9869d330ca32d0fe445
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-03 03:01:26 +0200

    Reordering statistics building launchers

--- bin/build_all.sh
+++ bin/build_all.sh
@@ -1,12 +1,12 @@
 # web frameworks
 bash bin/go_frameworks.sh
+bash bin/python_frameworks.sh
+bash bin/clojure_frameworks.sh
+bash bin/erlang_frameworks.sh
 bash bin/haskell_frameworks.sh
 bash bin/java_frameworks.sh
 bash bin/js_frameworks.sh
 bash bin/php_frameworks.sh
-bash bin/python_frameworks.sh
-bash bin/clojure_frameworks.sh
-bash bin/erlang_frameworks.sh
 bash bin/all_frameworks.sh
 # cms
 bash bin/php_cms.sh
