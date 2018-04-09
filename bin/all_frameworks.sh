2ee47dd4428a82d7a92e518cb45205b1263c44f5
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-09 02:42:59 +0200

    Launchers formatting

--- bin/all_frameworks.sh
+++ bin/all_frameworks.sh
@@ -3,7 +3,8 @@ yesodweb/yesod,snapframework/snap-core,agrafix/Spock,transient-haskell/axiom,myf
 leafo/lapis,sailorproject/sailor,keplerproject/orbit,luvit/luvit,Fizzadar/Luapress,kernelsauce/turbo,mongrel2/Tir,appwilldev/moochine,\
 playframework/playframework,vaadin/framework,lets-blade/blade,ninjaframework/ninja,bootique/bootique,jooby-project/jooby,pippo-java/pippo,ratpack/ratpack,primefaces/primefaces,netty/netty,\
 angular/angular,facebook/react,vuejs/vue,jquery/jquery,emberjs/ember.js,jashkenas/backbone,meteor/meteor,ractivejs/ractive,knockout/knockout,hyperapp/hyperapp,developit/preact,riot/riot,\
-laravel/framework,symfony/symfony,yiisoft/yii2,bcit-ci/CodeIgniter,bottlepy/bottle,plotly/dash,django/django,pallets/flask,Pylons/pyramid,channelcat/sanic,tornadoweb/tornado,web2py/web2py,TurboGears/tg2,\
+laravel/framework,symfony/symfony,yiisoft/yii2,bcit-ci/CodeIgniter,\
+bottlepy/bottle,plotly/dash,django/django,pallets/flask,Pylons/pyramid,channelcat/sanic,tornadoweb/tornado,web2py/web2py,TurboGears/tg2,\
 camping/camping,soveran/cuba,patriciomacadden/hobbit,hanami/hanami,ruby-hyperloop/hyper-react,padrino/padrino-framework,pakyow/pakyow,rack-app/rack-app,ramaze/ramaze,jeremyevans/roda,rails/rails,wardrop/Scorched,sinatra/sinatra,voltrb/volt,\
 SergioBenitez/rocket,iron/iron,actix/actix-web,gotham-rs/gotham,nickel-org/nickel.rs,Ogeon/rustful,rustless/rustless,tomaka/rouille,sappworks/sapper,mehcode/shio-rs,\
 tsujigiri/axiom,ChicagoBoss/ChicagoBoss,ninenines/cowboy,kivra/giallo,mochi/mochiweb,synrc/n2o,nitrogen/nitrogen,zotonic/zotonic,nova-framework/framework,\
