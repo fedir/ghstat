fd418fc502e277a94755be3e4260e9473944ec10
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-10 23:45:53 +0200

    Add more Java CMS

--- bin/java_cms.sh
+++ bin/java_cms.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ghstat -r dotCMS/core,alkacon/opencms-core,gentics/mesh,Softmotions/ncms,liferay/liferay-portal,bogeblad/infoglue,nuxeo/nuxeo -f stats/java_cms.csv
+./ghstat -r dotCMS/core,alkacon/opencms-core,gentics/mesh,Softmotions/ncms,liferay/liferay-portal,bogeblad/infoglue,nuxeo/nuxeo,lutece-platform/lutece-core,alkacon/opencms-core,exoplatform/ecms -f stats/java_cms.csv
