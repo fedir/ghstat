b4f907484df9e62085010fe935409e7bc034fcfc
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-11 20:25:50 +0200

    All CMS transversal statistics

---
+++ bin/all_cms.sh
@@ -0,0 +1,10 @@
+./ghstat -r \
+dotCMS/core,alkacon/opencms-core,gentics/mesh,Softmotions/ncms,liferay/liferay-portal,\
+bogeblad/infoglue,nuxeo/nuxeo,lutece-platform/lutece-core,alkacon/opencms-core,exoplatform/ecms,\
+Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,\
+forkcms/forkcms,getgrav/grav,joomla/joomla-cms,octobercms/october,pagekit/pagekit,redkite-labs/RedKiteCms,roadiz/roadiz,sulu/sulu-standard,\
+spip/SPIP,neos/neos-development-collection,WordPress/WordPress,modxcms/revolution,novius-os/novius-os,\
+LavaLite/cms,picocms/Pico,daylightstudio/FUEL-CMS,thelia/thelia,typicms/base,AsgardCms/Platform,odirleiborgert/borgert-cms,redaxscript/redaxscript,getkirby/starterkit,processwire/processwire,\
+symfony-cmf/symfony-cmf,zikula/core,TYPO3/TYPO3.CMS,drupal/drupal,\
+keystonejs/keystone,Dynalon/mdwiki,directus/directus,strapi/strapi,netlify/netlify-cms,sindresorhus/awesome,apostrophecms/apostrophe\
+ -f stats/all_cms.csv
