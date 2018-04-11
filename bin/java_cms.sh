5b1d974108f3e2422df00c832937f0ec9747f64b
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-11 20:26:49 +0200

    Launchers formatting

--- bin/java_cms.sh
+++ bin/java_cms.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./ghstat -r dotCMS/core,alkacon/opencms-core,gentics/mesh,Softmotions/ncms,liferay/liferay-portal,bogeblad/infoglue,nuxeo/nuxeo,lutece-platform/lutece-core,alkacon/opencms-core,exoplatform/ecms -f stats/java_cms.csv
+./ghstat -r dotCMS/core,alkacon/opencms-core,gentics/mesh,Softmotions/ncms,liferay/liferay-portal,\
+bogeblad/infoglue,nuxeo/nuxeo,lutece-platform/lutece-core,alkacon/opencms-core,exoplatform/ecms -f stats/java_cms.csv
--- bin/php_cms.sh
+++ bin/php_cms.sh
@@ -1,6 +1,3 @@
-#./ghstat -r TYPO3/TYPO3.CMS,Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,drupal/drupal,ezsystems/ezplatform,\
-#forkcms/forkcms,getgrav/grav,joomla/joomla-cms,octobercms/october,pagekit/pagekit,redkite-labs/RedKiteCms,roadiz/roadiz,sulu/sulu-standard,\
-#symfony-cmf/symfony-cmf,zikula/core -f stats/php_cms.csv
 ./ghstat -r Victoire/victoire,backbee/backbee-php,bolt/bolt,concrete5/concrete5,contao/core,\
 forkcms/forkcms,getgrav/grav,joomla/joomla-cms,octobercms/october,pagekit/pagekit,redkite-labs/RedKiteCms,roadiz/roadiz,sulu/sulu-standard,\
 spip/SPIP,neos/neos-development-collection,WordPress/WordPress,modxcms/revolution,novius-os/novius-os,\
