048aeb2766640ec043001358850f2aa8f1b3b430
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-15 15:58:09 +0200

    Move Graile to groovy. Add Spring.

--- bin/java_frameworks.sh
+++ bin/java_frameworks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "## Java frameworks statistics rating"
 echo ""
-./ghstat -r grails/grails-core,playframework/playframework,vaadin/framework,lets-blade/blade,ninjaframework/ninja,bootique/bootique,jooby-project/jooby,pippo-java/pippo,ratpack/ratpack,primefaces/primefaces,netty/netty -f stats/java_frameworks.csv
+./ghstat -r playframework/playframework,vaadin/framework,lets-blade/blade,ninjaframework/ninja,bootique/bootique,jooby-project/jooby,pippo-java/pippo,ratpack/ratpack,primefaces/primefaces,netty/netty,spring-projects/spring-framework -f stats/java_frameworks.csv
 echo "[Detailed Java frameworks statistics with ratings](https://github.com/fedir/ghstat/blob/master/stats/java_frameworks.csv)"
 echo ""
 
