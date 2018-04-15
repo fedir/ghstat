348dff72252ebe41fa8c7ea658718222b19bfbef
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-15 15:58:20 +0200

    Minor formatting

--- bin/java_cms.sh
+++ bin/java_cms.sh
@@ -4,4 +4,3 @@ echo ""
 ./ghstat -r dotCMS/core,alkacon/opencms-core,gentics/mesh,Softmotions/ncms,liferay/liferay-portal,\
 bogeblad/infoglue,nuxeo/nuxeo,lutece-platform/lutece-core,alkacon/opencms-core,exoplatform/ecms -f stats/java_cms.csv
 echo "[Detailed Java CMS statistics with ratings](https://github.com/fedir/ghstat/blob/master/stats/java_cms.csv)"
-echo ""
