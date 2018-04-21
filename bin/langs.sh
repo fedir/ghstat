909e6ed7dd19c1a5f931a36285e11332f387cef4
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-04-21 16:39:46 +0200

    Add Rust, Crystal, Lua, Erlang, Haskell, D languages

--- bin/langs.sh
+++ bin/langs.sh
@@ -1,6 +1,6 @@
 echo "## Programming languages rating"
 echo ""
-./ghstat -r php/php-src,python/cpython,ruby/ruby,golang/go -f stats/langs.csv
+./ghstat -r php/php-src,python/cpython,ruby/ruby,golang/go,rust-lang/rust,crystal-lang/crystal,lua/lua,erlang/otp,dlang/dmd -f stats/langs.csv
 echo "[Detailed programming languages statistics with ratings](https://github.com/fedir/ghstat/blob/master/stats/langs.csv)"
 echo ""
 
