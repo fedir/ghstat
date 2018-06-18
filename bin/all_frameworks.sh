1a1e61217d368250f2ff34b336bc85f121ec8c8e
Fedir RYKHTIK <fedir.rykhtik@gmail.com>
2018-06-18 02:01:18 +0200

    balderdashy/sails added

--- bin/all_frameworks.sh
+++ bin/all_frameworks.sh
@@ -8,7 +8,7 @@ analogweb/analogweb-scala,mesosphere/chaos,tumblr/colossus,twitter/finatra,lift/
 yesodweb/yesod,snapframework/snap-core,agrafix/Spock,transient-haskell/axiom,myfreeweb/magicbane,positiondev/fn,\
 leafo/lapis,sailorproject/sailor,keplerproject/orbit,luvit/luvit,Fizzadar/Luapress,kernelsauce/turbo,mongrel2/Tir,appwilldev/moochine,\
 playframework/playframework,vaadin/framework,lets-blade/blade,ninjaframework/ninja,bootique/bootique,jooby-project/jooby,pippo-java/pippo,ratpack/ratpack,primefaces/primefaces,netty/netty,spring-projects/spring-framework,eclipse/vert.x,\
-angular/angular,facebook/react,vuejs/vue,jquery/jquery,emberjs/ember.js,jashkenas/backbone,meteor/meteor,ractivejs/ractive,knockout/knockout,hyperapp/hyperapp,developit/preact,\
+angular/angular,facebook/react,vuejs/vue,jquery/jquery,emberjs/ember.js,jashkenas/backbone,meteor/meteor,ractivejs/ractive,knockout/knockout,hyperapp/hyperapp,developit/preact,balderdashy/sails,\
 expressjs/express,hexojs/hexo,hyperapp/hyperapp,MithrilJS/mithril.js,totaljs/framework,enyojs/enyo,microjs/microjs.com,nuxt/nuxt.js,riot/riot,\
 laravel/framework,symfony/symfony,yiisoft/yii2,bcit-ci/CodeIgniter,nova-framework/framework,cakephp/cakephp,pradosoft/prado,phalcon/cphalcon,nette/nette,PHPixie/Project,slimphp/Slim,leocavalcante/siler,\
 bottlepy/bottle,plotly/dash,django/django,pallets/flask,Pylons/pyramid,channelcat/sanic,tornadoweb/tornado,web2py/web2py,TurboGears/tg2,\
--- bin/js_frameworks.sh
+++ bin/js_frameworks.sh
@@ -7,6 +7,6 @@ meteor/meteor,ractivejs/ractive,\
 knockout/knockout,hyperapp/hyperapp,developit/preact,\
 expressjs/express,hexojs/hexo,hyperapp/hyperapp,MithrilJS/mithril.js,\
 totaljs/framework,enyojs/enyo,microjs/microjs.com,\
-nuxt/nuxt.js,riot/riot -f stats/js_frameworks.csv
+nuxt/nuxt.js,riot/riot,balderdashy/sails -f stats/js_frameworks.csv
 echo "[Detailed JS frameworks statistics with ratings](https://github.com/fedir/ghstat/blob/master/stats/js_frameworks.csv)"
 echo ""
